74fe695c3ea374c77e5769f878c6c95b204b096a
avt2255 <avt5067@gmail.com>
2023-03-30 17:18:57 +0530

    authentication

--- client/src/components/GoogleLogin.jsx
+++ client/src/components/GoogleLogin.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default function GoogleLogin() {
+   
+    const google = ()=>{
+        window.open("http://localhost:3001/auth/google","_self")
+    }
+
     return (
         <>
             <div className="col-lg-6">
-                <button className="btn btn-primary login-btn" type="submit">
+                <button className="btn btn-primary login-btn" type="submit" onClick={google}>
                     <i className="fa fa-google" style={{ fontSize: 18 }} />{" "}
                     Continue With Google
                 </button>
